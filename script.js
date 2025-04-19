01da29721b18c9ee280270d44d9983859113a00b
Sev A <lovluk2@gmail.com>
2025-04-19 13:23:01 -0400

    boiler plate web site template

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("clickMeBtn");
+    
+    button.addEventListener("click", () => {
+      alert("Hello! You clicked the button!");
+    });
+  });
+  
